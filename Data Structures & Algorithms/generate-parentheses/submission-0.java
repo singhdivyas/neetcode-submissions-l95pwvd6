db3895c53598d5d6b17cb15575b53baa0f229d50
singhdivyas <44027709+singhdivyas@users.noreply.github.com>
2026-04-18 15:24:10 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public boolean valid(String s) {
+        int open = 0;
+        for (char c : s.toCharArray()) {
+            open += c == '(' ? 1 : -1;
+            if (open < 0) return false;
+        }
+        return open == 0;
+    }
+
+    void dfs(String s, List<String> res, int n) {
+        if (n * 2 == s.length()) {
+            if (valid(s)) res.add(s);
+            return;
+        }
+        dfs(s + '(', res, n);
+        dfs(s + ')', res, n);
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        dfs("", res, n);
+        return res;
+    }
+}
